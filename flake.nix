5d81e54e04faa18641557ef761165732ef6e7e82
yueyinqiu <yueyinqiu@outlook.com>
2026-08-02 17:46:35 +0800

    f

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs }: let
+  outputs = { nixpkgs, ... }: let
     supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
   in {
