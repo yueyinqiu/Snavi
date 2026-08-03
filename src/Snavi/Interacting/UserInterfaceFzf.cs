9f863f2783e62b6b7904af4dc5c0f20dacacd0bf
yueyinqiu <yueyinqiu@outlook.com>
2026-08-03 23:14:24 +0800

    highlight

--- src/Snavi/Interacting/UserInterfaceFzf.cs
+++ src/Snavi/Interacting/UserInterfaceFzf.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using CliWrap;
 using CliWrap.Buffered;
 
@@ -19,10 +20,11 @@ sealed class UserInterfaceFzf : IUserInterface
         var command = Cli.Wrap("fzf");
         command = command.WithValidation(CommandResultValidation.None);
         command = command.WithArguments([
-            "--header", title.String,
+            "--ansi",
+            "--header", ToAnsiString(title),
 
             "--accept-nth", "1",
-            "--with-nth", "{2} {3}",
+            "--with-nth", "{2}        {3}",
             "--delimiter", delimiter
         ]);
         command = command.WithStandardInputPipe(PipeSource.FromString(
@@ -52,20 +54,34 @@ sealed class UserInterfaceFzf : IUserInterface
         command = command.WithValidation(CommandResultValidation.None);
         command = command.WithArguments([
             "--print-query",
-            "--header", title.String,
+            "--ansi",
+            "--header", ToAnsiString(title),
 
-            "--with-nth", "2",
+            "--accept-nth", "1",
+            "--with-nth", "{1}        {2}",
             "--delimiter", delimiter
         ]);
         var lines = await suggestions
             .Select(s => $"{s.Value}{delimiter}{s.Description}")
             .ToArrayAsync();
-        command = command.WithStandardInputPipe(PipeSource.FromString(string.Join(Environment.NewLine,lines)));
+        command = command.WithStandardInputPipe(PipeSource.FromString(string.Join(Environment.NewLine, lines)));
         var output = await command.ExecuteBufferedAsync(cancellationToken);
         var outputLines = output.StandardOutput.Split(Environment.NewLine);
         var query = outputLines[0];
         if (query.Length == 0 && outputLines.Length > 1)
-            return outputLines[1].Split(delimiter, 2)[0];
+            return outputLines[1];
         return query;
+    }
+
+    private static string ToAnsiString(IHighlightedString highlightedString)
+    {
+        var (offset, length) = highlightedString.Highlight.GetOffsetAndLength(highlightedString.String.Length);
+        var builder = new StringBuilder();
+        builder.Append(highlightedString.String, 0, offset);
+        builder.Append("\x1b[1m");
+        builder.Append(highlightedString.String, offset, length);
+        builder.Append("\x1b[0m");
+        builder.Append(highlightedString.String, offset + length, highlightedString.String.Length - offset - length);
+        return builder.ToString();
     }
 }
