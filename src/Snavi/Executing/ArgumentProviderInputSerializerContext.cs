2ca7b0d239ed2645e5d779fec2acdcfbcb867ea2
yueyinqiu <yueyinqiu@outlook.com>
2026-08-04 13:24:40 +0800

    better serialize

--- src/Snavi/Executing/ArgumentProviderInputSerializerContext.cs
+++ src/Snavi/Executing/ArgumentProviderInputSerializerContext.cs
@@ -2,6 +2,11 @@ using System.Text.Json.Serialization;
 
 namespace Snavi.Executing;
 
+[JsonSourceGenerationOptions(
+    AllowTrailingCommas = true,
+    ReadCommentHandling = System.Text.Json.JsonCommentHandling.Skip,
+    RespectNullableAnnotations = true
+)]
 [JsonSerializable(typeof(ArgumentProviderInput))]
 partial class ArgumentProviderInputSerializerContext : JsonSerializerContext
 {
--- src/Snavi/Modeling/CheatFileSerializerContext.cs
+++ src/Snavi/Modeling/CheatFileSerializerContext.cs
@@ -2,6 +2,11 @@ using System.Text.Json.Serialization;
 
 namespace Snavi.Modeling;
 
+[JsonSourceGenerationOptions(
+    AllowTrailingCommas = true,
+    ReadCommentHandling = System.Text.Json.JsonCommentHandling.Skip,
+    RespectNullableAnnotations = true
+)]
 [JsonSerializable(typeof(CheatFile))]
 partial class CheatFileSerializerContext : JsonSerializerContext
 {
