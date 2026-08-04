c5fcfc28f14a844a6f142d106051d9ff73df3a97
yueyinqiu <yueyinqiu@outlook.com>
2026-08-04 19:24:20 +0800

    add checkout json

---
+++ sample-cheats/git/list-branches.cs
@@ -0,0 +1,30 @@
+#:package Snavi.ArgumentSuggester@0.0.1
+#:package CliWrap@3.10.4
+
+using System.Runtime.CompilerServices;
+using CliWrap;
+using CliWrap.Buffered;
+using Snavi.ArgumentSuggester;
+
+await new Suggester().RunAsync();
+
+class Suggester : SnaviArgumentSuggester
+{
+    public override async IAsyncEnumerable<(string Value, string Description)> SuggestAsync(
+        IReadOnlyList<string> givenArguments,
+        DirectoryInfo currentDirectory,
+        DirectoryInfo temporaryDirectory,
+        [EnumeratorCancellation] CancellationToken cancellationToken
+    )
+    {
+        var output = await Cli.Wrap("git")
+            .WithArguments(["branch"])
+            .ExecuteBufferedAsync(cancellationToken);
+        foreach (var line in output.StandardOutput.Split(Environment.NewLine))
+        {
+            if (string.IsNullOrWhiteSpace(line))
+                continue;
+            yield return (line.Split(" ").Last(), "");
+        }
+    }
+}
